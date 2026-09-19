4230dd27d8d0cecc055dde41aac2544b936ad774
Alim <alimimanmalik2@gmail.com>
2026-09-19 17:34:23 +0500

    added simple links

--- server.js
+++ server.js
@@ -6,11 +6,16 @@ const app = express();
 
 const engine = new Liquid({
     root: [
+        path.resolve(__dirname, 'templates'),
         path.resolve(__dirname, 'layout'),
-        path.resolve(__dirname, 'sections'),
         path.resolve(__dirname, 'snippets'),
+        path.resolve(__dirname, 'sections')
     ],
-    extname: '.liquid'
+    layouts: path.resolve(__dirname, 'layout'),
+    extname: '.liquid',
+    globals: {
+        default_layout: 'theme.liquid'
+    }
 });
 
 engine.registerFilter('asset_url', (filename) => {
@@ -23,17 +28,21 @@ engine.registerFilter('stylesheet_tag', (url) => {
 });
 
 app.engine('liquid', engine.express());
-app.set('views', [
-    path.resolve(__dirname, 'layout'),
-    path.resolve(__dirname, 'sections'),
-    path.resolve(__dirname, 'snippets'),
-]);
+app.set('views', path.resolve(__dirname, 'templates'));
 app.set('view engine', 'liquid');
 
 app.use('/assets', express.static(path.resolve(__dirname, 'assets')));
 
 app.get('/', (req, res) => {
-    res.render('theme', { name: 'Alim', userRole: 'admin' });
+    res.render('index', {});
+});
+
+app.get('/full-portfolio', (req, res) => {
+    res.render('full-portfolio', {});
+});
+
+app.get('/contacts', (req, res) => {
+    res.render('contacts', {});
 });
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
